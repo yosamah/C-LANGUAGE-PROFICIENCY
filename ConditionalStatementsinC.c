e26bbe12736ab59e56253e8803373520c2225ce0
yosamah <yomna.osamma@gmail.com>
2020-07-04 19:40:22 +0200

    Adding  Conditional Statements in C problem

---
+++ ConditionalStatementsinC.c
@@ -0,0 +1,42 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main()
+{
+    int x;
+    scanf(" %d",&x);
+    if (x==1){
+        printf("one \n");
+    }else if (x==2){
+        printf("two \n");
+    }else if (x==3){
+        printf("three \n");
+    }else if (x==4){
+        printf("four \n");
+    }else if (x==5){
+        printf("five \n");
+    }else if (x== 6){
+        printf("six \n");
+    }else if (x==7 ){
+        printf("seven \n");
+    }else if (x==8){
+        printf("eight \n");
+    }else if (x==9){
+        printf("nine \n");
+    }else {
+        printf("Greater than 9 \n");
+    }
+    // Write Your Code Here
+
+    return 0;
+}
+
